796ac05a37f46429b8e4dd08d33e4ce1a3e08592
= <=>
2022-09-01 14:28:12 +0800

    [Feat] add frontend basic setting

---
+++ BunnyCode/README.md
@@ -0,0 +1,1 @@
+# BunnyCode frontend repo
---
+++ BunnyCode/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({
+  socket: Object,
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
