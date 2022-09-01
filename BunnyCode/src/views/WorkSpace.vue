8f208afcfd03cb0c16d6c50438ff696ad512625d
= <=>
2022-09-01 23:43:54 +0800

    [Feat] add backspace event

--- BunnyCode/src/views/WorkSpace.vue
+++ BunnyCode/src/views/WorkSpace.vue
@@ -1,12 +1,10 @@
 <script setup>
 import { onMounted, onUpdated, ref } from "vue";
 
-defineProps({
+const props = defineProps({
   socket: Object,
 });
 
-//TODO: create a space for user to input data (a list of input data) --> v-model + v-for
-// const input = ref([null]);
 const input = ref([]);
 let targetLine = ref(0);
 let prevCodes = [""];
@@ -14,49 +12,96 @@ const currCodes = ref([""]);
 const codeRecords = ref([]);
 
 const addCode = (e) => {
-  if(codeRecords.value.length > 50){
+  if (codeRecords.value.length > 50) {
     alert("記得儲存程式碼！");
   }
 
   // Binary check the index of input code.
-  let prev = prevCodes[targetLine.value].split("");
+  let prev = prevCodes.split("");
   const curr = currCodes.value[targetLine.value].split("");
   let result = {};
-  for(let i = 0; i < prev.length; i++){
-    if(prev[i] !== curr[i]){
+  if (prev.length > curr.length) {
+    console.log("backspace");
+    if (prevCodes == "") {
+      return;
+    }
+    for (let i = 0; i < curr.length; i++) {
+      if (prev[i] !== curr[i]) {
+        result = {
+          index: i,
+          keyword: prev[i],
+        };
+        break;
+      }
+    }
+    if (!result.keyword) {
       result = {
-        index: i,
-        keyword: curr[i],
-      };
-      break;
+        index: prev.length - 1,
+        keyword: prev[prev.length - 1],
+      }
     }
-  }
-  if(!result.keyword){
-    result = {
-      index: curr.length-1,
-      keyword: curr[curr.length - 1],
+    codeRecords.value.push({
+      type: "delete",
+      line: targetLine.value,
+      index: result.index,
+      code: result.keyword,
+      timestamp: Date.now().toString() + "000000",
+    });
+  } else if (prev.length < curr.length) {
+    for (let i = 0; i < prev.length; i++) {
+      if (prev[i] !== curr[i]) {
+        result = {
+          index: i,
+          keyword: curr[i],
+        };
+        break;
+      }
     }
+    if (!result.keyword) {
+      result = {
+        index: curr.length-1,
+        keyword: curr[curr.length - 1],
+      }
+    }
+    codeRecords.value.push({
+      type: "create",
+      line: targetLine.value,
+      index: result.index,
+      code: result.keyword,
+      timestamp: Date.now().toString() + "000000",
+    });
   }
-
-  codeRecords.value.push({
-    index: result.index,
-    code: result.keyword,
-    timestamp: Date.now().toString() + "000000",
-  });
-  const currentValue = currCodes.value[targetLine];
-  prevCodes[targetLine] = currentValue;
+  const currentValue = currCodes.value[targetLine.value];
+  console.log(codeRecords.value);
+  prevCodes = currentValue;
 }
 
 const checkEvent = (e) => {
-  if(e.keyCode === 13){
+  if (e.keyCode === 13) {
     console.log("enter");
     currCodes.value.push("");
     targetLine.value = currCodes.value.length - 1;
+  } else if (e.keyCode === 38) {
+    console.log("down");
+    if (targetLine.value > 0) {
+      targetLine.value = targetLine.value - 1;
+      input.value[targetLine.value].focus()
+    }
+  } else if (e.keyCode === 40) {
+    console.log("up");
+    if (targetLine.value < currCodes.value.length -1) {
+      targetLine.value = targetLine.value + 1
+      input.value[targetLine.value].focus()
+    }
+  } else if (e.ctrlKey && e.keyCode === 83) {
+    props.socket.emit("save", `Saving data ${JSON.stringify(codeRecords.value)}`)
+    console.log("Control + Save")
   }
-}
+};
 
 const changeTarget = (e) => {
   targetLine.value = Number(e.target.id.split("-")[1]);
+  prevCodes = currCodes.value[targetLine.value];
   console.log(targetLine.value);
 }
 
@@ -69,13 +114,6 @@ onMounted(() => {
 onUpdated(() => {
   input.value[targetLine.value].focus()
 })
-//TODO: 紀錄輸入在該行的位置
-
-//TODO: 紀錄輸入在的行數
-
-//TODO: Watch currCodes, when user add enter (keyboard event), list of input +1
-
-//TODO: Event: when user add enter, add new input html tag.
 
 //TODO: Event: When user add ctrl+s (keyboard event), send axios to backend
 </script>
@@ -84,7 +122,9 @@ onUpdated(() => {
   <ul>
     <li v-for="(code) in codeRecords">{{code.code}}</li>
   </ul>
-  <input v-for="(code, index) in currCodes" :id="`code-${index.toString()}`" ref="input" v-model="currCodes[index]" type="text" @keydown="checkEvent" @input="addCode" @click="changeTarget">
+  <div v-for="(code, index) in currCodes">
+    <input :id="`code-${index.toString()}`" ref="input" v-model="currCodes[index]" type="text" @keydown="checkEvent" @input="addCode" @click="changeTarget">
+  </div>
 </template>
 
 
