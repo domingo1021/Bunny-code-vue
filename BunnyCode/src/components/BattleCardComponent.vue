e238c21aaa5b0b40e2680eee04422db94e9d892a
= <=>
2022-09-19 21:11:16 +0800

    [Feat] add second user win battle card

--- BunnyCode/src/components/BattleCardComponent.vue
+++ BunnyCode/src/components/BattleCardComponent.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="card" style="width: 18rem">
     <div class="card-body">
-      <h5 class="card-title">{{battleObject.battleName}}</h5>
+      <h5 class="card-title">{{ battleObject.battleName }}</h5>
       <div class="flex-box">
-        <div class="win" v-if="battleObject.firstUserID === battleObject.winnerID">勝</div>
-        <img :src="battleObject.firstUserPicture" alt="picture-1">
+        <div
+          class="first-win"
+          v-if="battleObject.firstUserID === battleObject.winnerID"
+        >
+          勝
+        </div>
+        <img :src="battleObject.firstUserPicture" alt="picture-1" />
         <div>&nbsp;</div>
-        <div class="win" v-if="battleObject.secondUserID === battleObject.winnerID">勝</div>
-        <img :src="battleObject.secondUserPicture" alt="picture-1">
+        <div
+          class="second-win"
+          v-if="battleObject.secondUserID === battleObject.winnerID"
+        >
+          勝
+        </div>
+        <img :src="battleObject.secondUserPicture" alt="picture-1" />
       </div>
       <div class="flex-box">
         <h6 class="card-subtitle mb-2">
-          <div class="name">{{battleObject.firstUserName}}</div>
+          <div class="name">{{ battleObject.firstUserName }}</div>
         </h6>
         <div>
-          <div> v.s.</div>
+          <div>v.s.</div>
         </div>
         <h6 class="card-subtitle mb-2">
-          <div class="name">{{battleObject.secondUserName}}</div>
+          <div class="name">{{ battleObject.secondUserName }}</div>
         </h6>
       </div>
     </div>
@@ -26,24 +36,24 @@
 
 <script setup>
 defineProps({
-  battleObject: Object
-})
+  battleObject: Object,
+});
 </script>
 
 <style scoped>
-.card{
+.card {
   background-color: #21262d;
   border: 0.3px solid #85888b;
-  height: 100%
+  height: 100%;
 }
-.card:hover{
+.card:hover {
   cursor: pointer;
   background-color: rgb(69, 75, 103);
 }
-.name{
+.name {
   margin-top: 10px;
 }
-.flex-box{
+.flex-box {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -51,7 +61,7 @@ defineProps({
   margin-left: 10%;
 }
 
-.win{
+.first-win {
   z-index: 99;
   position: absolute;
   left: 20%;
@@ -61,7 +71,17 @@ defineProps({
   height: 25px;
 }
 
-img{
+.second-win {
+  z-index: 99;
+  position: absolute;
+  left: 93%;
+  background-color: rgb(228, 62, 62);
+  padding: 2%;
+  border-radius: 50%;
+  height: 25px;
+}
+
+img {
   width: 50px;
   height: 50px;
   border-radius: 50%;
